32325cf45551223ce7a5115403f908b520e687fc
bagel786 <safbaig5@gmail.com>
2026-02-06 22:17:45 -0600

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Safiullah Baig" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi guys, I'm Safiullah Baig, but I go by Saf. I collect pokemon cards, and I have a substantial lego collection, mostly consisting of Ninjago and car sets. My favorite set I own is the Temple of Airjitzu. My favorite games are roleplay/adventure games like Pokemon and Silksong. I also enjoy 5v5 laning games like Mobile Legends and Pokemon Unite. My favorite non-color is holo. ")
 
 if __name__ == "__main__":
     main()
